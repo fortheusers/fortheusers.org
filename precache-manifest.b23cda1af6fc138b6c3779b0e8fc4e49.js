ca9a1a9a203fc6902a77d399de1d33e30e74eda6
vgmoose <me@vgmoose.com>
2019-10-21 22:52:14 -0400

    appstore site

---
+++ precache-manifest.b23cda1af6fc138b6c3779b0e8fc4e49.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "940f2d3907fdd7c868c3",
+    "url": "/static/css/main.9e805a52.chunk.css"
+  },
+  {
+    "revision": "940f2d3907fdd7c868c3",
+    "url": "/static/js/main.940f2d39.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "c54ea462b1b7697254ed",
+    "url": "/static/js/2.c54ea462.chunk.js"
+  },
+  {
+    "revision": "cc38790579438a3b070a4e6229b83b0c",
+    "url": "/static/media/loader.cc387905.gif"
+  },
+  {
+    "revision": "0dc8ae074526d0475abd37893b0a547c",
+    "url": "/static/media/noscreen.0dc8ae07.png"
+  },
+  {
+    "revision": "7f2428495fc18979f15ac719c6f68826",
+    "url": "/index.html"
+  }
+];
